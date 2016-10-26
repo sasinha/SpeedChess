3a7d13f083cbb78e96230d8f749e59d01cf4d0b3
sasinha <sasinha@ucsc.edu>
2016-10-26 01:02:39 -0700

    Initial Hello World

--- app/src/main/java/sinhasanchit/speedchess/ChessTimer.java
+++ app/src/main/java/sinhasanchit/speedchess/ChessTimer.java
@@ -2,12 +2,17 @@ package sinhasanchit.speedchess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Timer;
 
-public class MainActivity extends AppCompatActivity {
+public class ChessTimer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
